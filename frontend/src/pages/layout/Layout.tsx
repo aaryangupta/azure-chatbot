63f1ba43aa2b2e116cfd61769382c8a397c2e90c
Aaryan gupta <aaryangupta2201@gmail.com>
2025-02-13 22:13:15 +0530

    ui update

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -90,7 +90,7 @@ const Layout = () => {
                 text={appStateContext?.state?.isChatHistoryOpen ? hideHistoryLabel : showHistoryLabel}
               />
             )}
-            {ui?.show_share_button && <ShareButton onClick={handleShareClick} text={shareLabel} />}
+            {/*ui?.show_share_button && <ShareButton onClick={handleShareClick} text={shareLabel} />*/}
           </Stack>
         </Stack>
       </header>
